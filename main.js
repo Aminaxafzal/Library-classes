50190a225fe960a6c174b9da3df098aee1262260
minamedia <mina.m3dia@gmail.com>
2021-06-29 13:26:17 +0100

    created-a-library-system-using-classes

--- main.js
+++ main.js
@@ -1,21 +1,95 @@
-class media {
+class Media {
+
+
     constructor(title) {
-      this._title = title, 
-      this._isCheckedOut = isCheckedOut,
-      this._ratings = rating 
+      this._title = title; 
+      this._isCheckedOut = false;
+      this._ratings = [];
     }
   
-    get _title() {
-      return this._title
+    get title() {
+      return this._title;
     }
-  
-      get _isCheckedOut() {
-      return this._isCheckedOut
+    get isCheckedOut() {
+      return this._isCheckedOut;
     }
-  
-      get _ratings() {
+    get ratings() {
       return this._ratings
     }
   
+    set isCheckedOut(value) {
+      this._isCheckedOut = value;
+    }
+  
+    toggleCheckOutStatus() {
+     this._isCheckedOut = !this._isCheckedOut; 
+      }
+  
+  getAverageRating() {
+   let ratingsSum = this.ratings.reduce((accumulator, rating) => accumulator + rating, 0);
+   return ratingsSum / this.ratings.length;
+  }
+  
+  addRating(value) {
+    this.ratings.push(value);
+   
+   }
+  }
+  
+  class Book extends Media {
+  
+    constructor(author, title, pages) {
+     super(title); //this is what lets you use the properties from the parent class
+     this._author = author;
+     this._pages = pages;
+  }
+  //always the same syntax just changing the values were using
+  get author() {
+      return this._author;
+    }
+  
+   get pages() {
+      return this._pages;
+    }
   
   }
+  
+  class Movie extends Media {
+    constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  
+    }
+  
+    get director() {
+      return this._director;
+    }
+  
+    get runTime() {
+      return this._runTime;
+    }
+  
+  }
+  
+  
+  const historyOfEverything = new Book ('Bill Bryson', 'A Short History of Nearly Everything', 544 ); 
+  
+  //testing 
+  historyOfEverything.toggleCheckOutStatus(); // have to call it to change status
+  console.log(historyOfEverything.isCheckedOut);
+  historyOfEverything.addRating(4);
+  historyOfEverything.addRating(5);
+  historyOfEverything.addRating(5);
+  console.log(historyOfEverything.getAverageRating());
+  
+  const speed = new Movie ('Jan de Bont', 'Speed', 116);
+  
+  //testing 
+  speed.toggleCheckOutStatus();
+  console.log(speed.isCheckedOut);
+  speed.addRating(1);
+  speed.addRating(5);
+  speed.addRating(5);
+  console.log(speed.getAverageRating());
+  
