70eec85c0465496f6bcd7956ab45c25ae811d30c
minamedia <mina.m3dia@gmail.com>
2021-06-29 10:25:14 +0100

    Created-html-and-parent-class

---
+++ main.js
@@ -0,0 +1,21 @@
+class media {
+    constructor(title) {
+      this._title = title, 
+      this._isCheckedOut = isCheckedOut,
+      this._ratings = rating 
+    }
+  
+    get _title() {
+      return this._title
+    }
+  
+      get _isCheckedOut() {
+      return this._isCheckedOut
+    }
+  
+      get _ratings() {
+      return this._ratings
+    }
+  
+  
+  }
